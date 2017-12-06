d20e7ada69d41c56ba92600b63dddf84a056f8d8
Clementina <mataclemy@gmail.com>
2017-12-06 11:47:11 +0100

    category and subcategory working

--- server/models/Product.js
+++ server/models/Product.js
@@ -6,18 +6,24 @@ const Schema   = mongoose.Schema;
 const productSchema = new Schema({
   brand: { type: String },
   model: { type: String },
-  category: { type: String, enum: ["Mod", "Ato", "E-liquid"], default:"Mod"},
-  subcategory: { type: String, enum: ["Mechanic", "Elecronic", "Atomizer", "Claromizer", "0gr", "3gr",] },
+  // category: { type: String, enum: ["Mod", "Ato", "E-liquid"], default:"Mod"},
+  // subcategory: { type: String, enum: ["Mechanic", "Elecronic", "Atomizer", "Claromizer", "0gr", "3gr",] },
   image: { type: String, default: "https://i.pinimg.com/736x/34/9f/14/349f149c034cc5355ea3c04c3384d3f3--amazing-pictures-hobby.jpg"},
   description: { type: String },
   price: { type: String },
   rating: { type: Number },
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+  category: { type:{
+   Mod: { type: String, enum:['Mechanic','Electronic'] },
+   Ato : { type: String, enum:['Atomizer','Claromizer'] },
+   Eliquid : { type: String, enum:['Nicotine','noNicotine'] }
+ }
+}
+},{
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
   }
-});
+  });
 
 const Product = mongoose.model('Product', productSchema);
 module.exports = Product;
--- server/routes/product.js
+++ server/routes/product.js
@@ -4,23 +4,25 @@ const Product = require('../models/Product');
 const mongoose = require('mongoose');
 
 
-
-
+//Create new product
 router.post('/product', (req, res, next) => {
   const newProduct = new Product({
     brand: req.body.brand,
     model: req.body.model,
     category: req.body.category,
-    subcategory: req.body.subcategory,
+    // subcategory: req.body.subcategory,
     image: req.body.image,
     description: req.body.description,
     price: req.body.price,
     rating: req.body.rating,
+
   });
 
 
 	newProduct.save()
-  .then( request => {res.json({ message: 'New Product created!', id: newProduct._id, price: newProduct.price, category: newProduct.category, subcategory: newProduct.subcategory});})
+  .then( request => {res.json({ message: 'New Product created!', id: newProduct._id, price: newProduct.price, category: newProduct.category
+  // , subcategory: newProduct.subcategory
+});})
   .catch( err => {res.json(err); });
 
 });
