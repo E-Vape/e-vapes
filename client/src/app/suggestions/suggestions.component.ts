b7b1037762fc5013669bd697ce51e033879ec218
Clementina <mataclemy@gmail.com>
2017-12-12 17:18:12 +0100

    suggestions

--- client/src/app/suggestions/suggestions.component.ts
+++ client/src/app/suggestions/suggestions.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suggestions.component.css']
 })
 export class SuggestionsComponent implements OnInit {
-  zoom: number = 25;
-  lat: number = 40.392467;
-  lng: number = -3.697553;
+
   constructor() { }
+
+  zoom: number = 13;
+  //Mapa1
+  lat1: number = 40.421623;
+  lng1: number = -3.720285;
+  //Mapa2
+  lat2: number = 40.425688;
+  lng2: number = -3.668940;
+  //Mapa3
+  lat3: number = 40.398565;
+  lng3: number = -3.705014;
+  //Mapa4
+  lat4: number = 40.345390;
+  lng4: number = -3.815870;
+  //Mapa5
+  lat5: number = 40.427278;
+  lng5: number = -3.670093;
 
   ngOnInit() {
   }
