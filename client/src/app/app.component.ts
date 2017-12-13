7bf577b28f4ab2104643542740bb8c21901e3ca2
Angel Sirodey Alonso <xiro@MacBook-Pro-de-Angel.local>
2017-12-13 13:17:52 +0100

    working in styles

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Injectable, Pipe, PipeTransform } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { ProductsService } from '../services/products.service';
+import { ShoppingCartService } from '../services/shopping-cart.service';
 
 
 
@@ -11,7 +13,7 @@ import { AuthService } from '../services/auth.service';
 })
 export class AppComponent {
   title = 'E-vapes';
-  constructor(public authService: AuthService) {}
+  constructor(public ProductsService: ProductsService, public authService: AuthService, public shoppingCartService: ShoppingCartService,) {}
 
   logout() {
     this.authService.logout().subscribe();
