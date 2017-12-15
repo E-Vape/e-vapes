49f3536f525a20eed175f3c835e37e0a6f638eeb
Clementina <mataclemy@gmail.com>
2017-12-15 09:29:48 +0100

    ready for the next depliyment

--- server/app.js
+++ server/app.js
@@ -61,7 +61,7 @@ app.use('/', review);
 app.use('/', address);
 app.use('/', cart);
 
-app.use((req, res) =>res.sendfile(__dirname + '/public/index.html'));
+app.all((req, res) =>res.sendfile(__dirname + '/public/index.html'));
 
 
 // catch 404 and forward to error handler
